13ecd5203bde0675c74222d6c27f93399baa466b
Dell <Dell@DESKTOP-76NCJ84>
2025-02-13 11:42:04 -0500

    Add project files.

---
+++ FeatureManagementAPI/Controllers/FeaturesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeatureManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FeaturesController : ControllerBase
+    {
+        public FeaturesController()
+        {
+           
+        }   
+    }
+
+    public class Feature
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string EstimatedComplexity { get; set; } // S, M, L, XL
+        public string Status { get; set; } // New, Active, Closed, Abandoned
+        public DateTime? TargetCompletionDate { get; set; }
+        public DateTime? ActualCompletionDate { get; set; }
+    }
+}
